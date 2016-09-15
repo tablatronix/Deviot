e6bb09ee88d4fd1e6df9fa8588c5bbfb22d82144
gepd <guillermoepd@hotmail.com>
2016-09-14 23:29:01 -0300

    Make "quick panel" persistent when focus is lost

--- libs/QuickPanel.py
+++ libs/QuickPanel.py
@@ -13,5 +13,7 @@ quick_panel_id = 0
 
 
 def quickPanel(items, callback, window=False, flags=0, index=0):
+    if(not flags):
+        flags = sublime.KEEP_OPEN_ON_FOCUS_LOST
     window = sublime.active_window()
     window.show_quick_panel(items, callback, flags, index)
